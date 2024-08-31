c3665eb3ed10f5afcff445334e14a5f21434deca
code-onigiri <shibamasa759@gmail.com>
2024-08-31 13:55:24 +0900

    add

---
+++ src/app/add/page.tsx
@@ -0,0 +1,9 @@
+function Add(){
+    return(
+        <>
+            <h1>残念だったな、これも作り途中だよ</h1>
+        </>
+    )
+}
+
+export default Add
---
+++ src/app/search/page.tsx
@@ -0,0 +1,9 @@
+function Search() {
+    return (
+        <>
+            <h1>残念だったな、作り途中だよ</h1>
+        </>
+    )
+}
+
+export default Search
